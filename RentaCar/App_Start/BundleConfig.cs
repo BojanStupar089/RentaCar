0d889f2d0c6d9d4033d842796df19de2bee21521
BojanStupar089 <bojanstupar089@gmail.com>
2022-12-26 15:58:40 +0100

    Updated bootstrap to version 5.2.3,  navbar and index.cshtml of HomeCoontrollers

--- RentaCar/App_Start/BundleConfig.cs
+++ RentaCar/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace RentaCar
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-def.css",
                       "~/Content/site.css"));
         }
     }
