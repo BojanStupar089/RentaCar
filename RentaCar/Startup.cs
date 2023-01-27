4e58563e89bffb8f48f402a9aeeef4cf6f705369
BojanStupar089 <bojanstupar089@gmail.com>
2023-01-27 08:02:08 +0100

    Updated Web.config added DefaultConnection for authentication Added static class Role Added method createRolesAndUsers in Startup.cs

--- RentaCar/Startup.cs
+++ RentaCar/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using RentaCar.Models;
 
-[assembly: OwinStartupAttribute(typeof(RentaCar.Startup))]
+[assembly:OwinStartupAttribute(typeof(RentaCar.Startup))]
 namespace RentaCar
 {
     public partial class Startup
@@ -9,6 +12,64 @@ namespace RentaCar
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
+        }
+
+        private void createRolesAndUsers()
+        {
+
+                 ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists(Role.Admin))
+            {
+
+                var role = new IdentityRole();
+                role.Name = Role.Admin;
+
+                roleManager.Create(role); /// napravio si role Admin.
+
+                // kreiras super admina
+                var user = new ApplicationUser();
+                user.UserName = "bokiadmin44@gmail.com";
+                user.Email = "bokiadmin44@gmail.com";
+                // user.LockoutEnabled = true;
+                string userPWD = "Bokica.89";
+
+                var chkUser = UserManager.Create(user, userPWD); // kreiran super admin
+
+                // add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+
+                    var result1 = UserManager.AddToRole(user.Id, Role.Admin);
+                }
+
+            }
+            else if (!roleManager.RoleExists(Role.Employee))
+            {
+
+                var role = new IdentityRole();
+                role.Name = Role.Employee;
+                roleManager.Create(role);
+            }
+
+        else //if(!roleManager.RoleExists(Role.Employee))
+            {
+
+
+                var role = new IdentityRole();
+                role.Name = Role.User;
+                roleManager.Create(role);
+
+            }
+
+
+             }
+
+
         }
     }
-}
