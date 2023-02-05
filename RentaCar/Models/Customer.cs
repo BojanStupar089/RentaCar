45be40df868dc51d089f4c8dfd983f23764f0548
BojanStupar089 <bojanstupar089@gmail.com>
2023-02-05 11:22:52 +0100

    Updated label DriverLicNo to Driver Licence Number

--- RentaCar/Models/Customer.cs
+++ RentaCar/Models/Customer.cs
@@ -11,7 +11,8 @@ namespace RentaCar.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Customer
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,6 +23,7 @@ namespace RentaCar.Models
     
         public int CustomerId { get; set; }
         public string Name { get; set; }
+        [Display(Name="Driver Licence Number")]
         public int DriverLicNo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
